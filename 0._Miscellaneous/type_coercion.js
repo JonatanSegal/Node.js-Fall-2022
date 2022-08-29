1eaee4053ea8f6f0405b9af0405581cdb659a238
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-08-29 09:56:50 +0200

    Added a miscellaneous folder with a typer coercion file and intro exercises folder

---
+++ 0._Miscellaneous/type_coercion.js
@@ -0,0 +1,9 @@
+// Type coersion lecture
+const total = 34.32;
+const price = 12.32;
+
+const totalPrice = total + price;
+console.log(totalPrice);
+
+const balanceEquals = total === totalPrice;
+console.log(balanceEquals)
