0a167f11943efdd2eb606b82898ee654ce380024
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-10-19 15:32:44 +0200

    More setup before coding

---
+++ Mandatory_1/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", server.address().port);
+});
