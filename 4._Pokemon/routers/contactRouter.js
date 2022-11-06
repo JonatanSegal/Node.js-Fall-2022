dcd7accacf8c32c2ac1872e6966ac8c566569ec6
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-11-06 15:03:04 +0100

    Nodemailer assignment for contact page

--- 4._Pokemon/routers/contactRouter.js
+++ 4._Pokemon/routers/contactRouter.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import { mailer } from "../util/nodeMailer.js";
 const router = Router();
 
 router.post("/api/contact", (req, res) => {
-    res.send(req.body);
+    console.log(req.body)
+    let sender = req.body.email
+    let message = req.body.message
+    let name = req.body.name
+    mailer(sender, message, name)
+    .then(result => {
+        console.log(result)
+        res.send(`<a href="${result}" target="_blank">Generated email</a><br><a href="/">Home</a>`)})
+    .catch(console.error)
 });
 
 export default router;
