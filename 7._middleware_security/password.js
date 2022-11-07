24f57215f5f104fe50d67077e5b8415fd975a3d8
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-11-07 11:28:16 +0100

    bcrypt

--- 7._middleware_security/password.js
+++ 7._middleware_security/password.js
@@ -1,3 +1,12 @@
 import bcrypt from "bcrypt"
 
 const saltRounds = 12
+const plaintextPassword = "Hunter12"
+const loginPassword = "Hunter12"
+const encryptedpassword = "$2b$12$Yyjz82XQMb0.PYj3ipiRz.GP2Azls1NLZAO4.4lPwvqBdf1G4dBgO"
+
+ const encryptedpasswordResult = await bcrypt.hash(plaintextPassword, saltRounds)
+ //console.log(encryptedpasswordResult)
+
+ const passwordComparison = await bcrypt.compare(loginPassword, encryptedpassword)
+console.log(passwordComparison)
