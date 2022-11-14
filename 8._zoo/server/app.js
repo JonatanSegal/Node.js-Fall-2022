b85bb08dd21e6e3f6e120108911665e663fe6a3a
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-11-14 11:43:21 +0100

    added sqlite database to zoo project, ddl to set up database, animal router that can add to database

--- 8._zoo/server/app.js
+++ 8._zoo/server/app.js
@@ -1,21 +1,20 @@
 import  express  from "express";
 const app = express()
 
+
 //import path from "path"
 // Ways to send file via express
 // res.sendFile()
 //app.use(express.static(path.resolve("../client/dist")))
+app.use(express.json())
 
 // Not safe but allows us to work around CORS problems
 import cors from "cors"
 app.use(cors())
 
+import animalsRouter from "./routers/animalsRouter.js"
+app.use(animalsRouter)
 
-const animals = ["🐒", "🦝", "🐼", "🦦"]
-
-app.get("/api/animals", (req, res) =>{
-    res.send({data: animals})
-})
 
 const PORT = 8080 || process.env.PORT
 app.listen(PORT, () =>console.log("App is running on: ", PORT) )
---
+++ 8._zoo/server/routers/animalsRouter.js
@@ -0,0 +1,26 @@
+import {Router} from "express"
+const router = Router()
+
+import db from "../database/connecttion_sqlite.js"
+
+//const animals = ["🐒", "🦝", "🐼", "🦦"]
+
+router.get("/api/animals", async (req, res) =>{
+    const data = await db.all("SELECT * FROM animals")
+    //console.log(data)
+    res.send({data})
+})
+
+router.post("/api/animals", async (req, res) => {
+    const commonName = req.body.commonName
+    const latinName = req.body.latinName || "Unknown"
+    if(!commonName) return res.status(400).send({message:"Common name not defined"})
+   
+    const result = await db.run(`INSERT INTO animals(latin_name, common_name) VALUES (?,?) `,[latinName, commonName])
+    console.log(result)
+
+    res.send({changes : result.changes})
+})
+
+
+export default router
