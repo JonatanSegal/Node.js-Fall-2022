44473ee625847de30522b963aa3a5f226e5a1dcd
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-12-05 10:11:56 +0100

    Sockets for plain text html

---
+++ 10._Sockets_Basic_HTML/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import http from "http"
+import {Server} from "socket.io"
+
+const app = express()
+app.use(express.static("public"))
+
+const server = http.createServer(app)
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+    console.log(`A socket connectd on id ${socket.id}`)
+
+    socket.on("client-choose-a-color", (data) => {
+        //emits a broadcast to all socket but itself
+        //socket.broadcast.emit("this is the new color", data)
+        
+        //emits only to the socket itself
+        //socket.emit("this is the new color", data)
+
+        //emits to all sockets in the io name space
+        io.emit("this is the new color", data)
+    })
+
+    io.on("disconnect", () => {
+        console.log(`Socket ${socket.id} left.`)
+    })
+})
+
+
+server.listen(8080, () => console.log(`Server is running on port`, 8080))
